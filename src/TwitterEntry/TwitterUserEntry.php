36c88dfe0be393e16cd4fd071067145dfa9c3ecb
JLueke <jac_lueken@live.de>
2016-12-31 21:02:47 +0100

    Remove mode dependencies from entry (#1)
    * Removed Recommendation from package
    
    * updated package names

--- src/TwitterEntry/TwitterUserEntry.php
+++ src/TwitterEntry/TwitterUserEntry.php
@@ -3,13 +3,12 @@
 namespace Publisher\Entry\Twitter;
 
 use Publisher\Entry\AbstractEntry;
-use Publisher\Mode\Recommendation\RecommendationInterface;
 use Publisher\Helper\Validator;
 
 /*
  * @link https://dev.twitter.com/rest/reference/post/statuses/update
  */
-class TwitterUserEntry extends AbstractEntry implements RecommendationInterface
+class TwitterUserEntry extends AbstractEntry
 {
     
     const MAX_LENGTH_OF_MESSAGE = 140;
@@ -24,8 +23,8 @@ class TwitterUserEntry extends AbstractEntry implements RecommendationInterface
     {
         Validator::checkRequiredParametersAreSet($body, array('status'));
         Validator::validateMessageLength(
-                $body['status'],
-                self::MAX_LENGTH_OF_MESSAGE
+            $body['status'],
+            self::MAX_LENGTH_OF_MESSAGE
         );
     }
     
@@ -35,22 +34,6 @@ class TwitterUserEntry extends AbstractEntry implements RecommendationInterface
     {
         $object = json_decode($response);
         return (isset($object->id_str));
-    }
-    
-    // Implementation of RecommendationInterface
-    
-    public function setRecommendationParameters(
-        string $message,
-        string $url = '',
-        string $title = '',
-        $date = null
-    ) {
-        $status = empty($title) ? $message : $title."\n".$message;
-        $status .= empty($url) ?  '' : "\n".$url;
-        
-        $this->setBody(array(
-            'status' => $status
-        ));
     }
 
 }
